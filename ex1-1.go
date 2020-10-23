a0c7838e2162a3dfb5bf582080489e14480fcfe6
Justin Flude <jflude@users.noreply.github.com>
2020-10-23 09:20:43 -0500

    Ex. 1-1

--- ex1-1.go
+++ ex1-1.go
@@ -1,9 +1,29 @@
 package main
 
-import "github.com/jflude/ppicg/grsys"
+import (
+	"fmt"
+	"github.com/jflude/ppicg/grsys"
+)
 
 func main() {
+	var xA, yA, xB, yB, xC, yC float32
+	if _, err := fmt.Scanln(&xA, &yA, &xB, &yB, &xC, &yC); err != nil {
+		grsys.ErrorMsg(err.Error())
+	}
+	var q float32 = 0.05
+	p := 1 - q
+
 	grsys.Initgr()
-	// TODO
+	for i := 0; i < 50; i++ {
+		grsys.Move(xA, yA)
+		grsys.Draw(xB, yB)
+		grsys.Draw(xC, yC)
+		grsys.Draw(xA, yA)
+
+		xT, yT := xA, yA
+		xA, yA = p*xA+q*xB, p*yA+q*yB
+		xB, yB = p*xB+q*xC, p*yB+q*yC
+		xC, yC = p*xC+q*xT, p*yC+q*yT
+	}
 	grsys.Endgr()
 }
