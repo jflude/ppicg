cf163d97fe2b30ff1919c1e217bbec23a4b05dfd
Justin Flude <jflude@users.noreply.github.com>
2020-10-23 14:04:24 -0500

    Ex. 1-3

---
+++ ex1-3.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"github.com/jflude/ppicg/grsys"
+)
+
+func main() {
+	fmt.Print("Enter the number of intervals for hatching: ")
+	var m int
+	if _, err := fmt.Scanln(&m); err != nil {
+		grsys.ErrorMsg(err.Error())
+	}
+
+	grsys.Initgr("")
+	var width, height, xOrg, yOrg float32
+	if grsys.ImageWidth > grsys.ImageHeight {
+		height = grsys.YMax - grsys.YMin
+		width = height
+		xOrg = (grsys.XMax - grsys.YMax) / 2
+	} else {
+		width = grsys.XMax - grsys.XMin
+		height = width
+		yOrg = (grsys.YMax - grsys.XMax) / 2
+	}
+	xOpp := xOrg + width
+	yOpp := yOrg + height
+
+	grsys.Move(xOrg, yOrg)
+	grsys.Draw(xOpp, yOrg)
+	grsys.Draw(xOpp, yOpp)
+	grsys.Draw(xOrg, yOpp)
+	grsys.Draw(xOrg, yOrg)
+
+	xSqInc := width / 8
+	ySqInc := height / 8
+	for x := xOrg; x <= xOpp; x += xSqInc {
+		grsys.Move(x, yOrg)
+		grsys.Draw(x, yOpp)
+	}
+	for y := yOrg; y <= yOpp; y += ySqInc {
+		grsys.Move(xOrg, y)
+		grsys.Draw(xOpp, y)
+	}
+
+	xHchInc := xSqInc / float32(m)
+	yHchInc := ySqInc / float32(m)
+	firstBlack := true
+	for ySq := yOrg; ySq < yOpp; ySq += ySqInc {
+		firstBlack = !firstBlack
+		isBlack := firstBlack
+		for xSq := xOrg; xSq < xOpp; xSq += xSqInc {
+			if isBlack = !isBlack; !isBlack {
+				continue
+			}
+			var i float32 = 0
+			for ; i < float32(m); i += 1 {
+				grsys.Move(xSq+i*xHchInc, ySq)
+				grsys.Draw(xSq+xSqInc, ySq+ySqInc-i*yHchInc)
+				grsys.Move(xSq, ySq+i*yHchInc)
+				grsys.Draw(xSq+xSqInc-i*xHchInc, ySq+ySqInc)
+			}
+		}
+	}
+	grsys.Endgr()
+}
