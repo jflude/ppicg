3c25b7f99b02988116a0106e731e531651b737ce
Justin Flude <jflude@users.noreply.github.com>
2020-10-24 12:33:13 -0500

    Cohen-Sutherland line clipping

---
+++ clipdemo.go
@@ -0,0 +1,44 @@
+// Demonstration of the Cohen-Sutherland line-clipping algorithm.
+package main
+
+import (
+	"github.com/jflude/ppicg/grsys"
+	"math"
+)
+
+func main() {
+	alpha := 72 * math.Pi / 180
+	phi0 := 0.0
+	grsys.Initgr("")
+
+	d := 0.1 * grsys.RMax
+	xmin := grsys.XMin + d
+	xmax := grsys.XMax - d
+	ymin := grsys.YMin + d
+	ymax := grsys.YMax - d
+
+	// The window is now drawn:
+	grsys.Move(xmin, ymin)
+	grsys.Draw(xmax, ymin)
+	grsys.Draw(xmax, ymax)
+	grsys.Draw(xmin, ymax)
+	grsys.Draw(xmin, ymin)
+	grsys.SetClipBoundaries(xmin, xmax, ymin, ymax)
+
+	// As far as permitted by the boundaries of the window,
+	// 20 concentric regular pentagons are drawn:
+	for j := 1; j <= 20; j++ {
+		r := float64(j) * d
+		x2 := grsys.XCenter + r*math.Cos(phi0)
+		y2 := grsys.YCenter + r*math.Sin(phi0)
+		for i := 1; i <= 5; i++ {
+			phi := phi0 + float64(i)*alpha
+			x1 := x2
+			y1 := y2
+			x2 = grsys.XCenter + r*math.Cos(phi)
+			y2 = grsys.YCenter + r*math.Sin(phi)
+			grsys.ClipDraw(x1, y1, x2, y2)
+		}
+	}
+	grsys.Endgr()
+}
---
+++ grsys/clip.go
@@ -0,0 +1,71 @@
+// Implementation of Cohen-Sutherland line clipping.
+package grsys
+
+var xMinClip, xMaxClip, yMinClip, yMaxClip float64
+
+func SetClipBoundaries(x1, x2, y1, y2 float64) {
+	xMinClip = x1
+	xMaxClip = x2
+	yMinClip = y1
+	yMaxClip = y2
+}
+
+func clipCode(x, y float64) int {
+	var c int
+	if x < xMinClip {
+		c |= 8
+	} else if x > xMaxClip {
+		c |= 4
+	}
+	if y < yMinClip {
+		c |= 2
+	} else if y > yMaxClip {
+		c |= 1
+	}
+	return c
+}
+
+func ClipDraw(xP, yP, xQ, yQ float64) {
+	cP := clipCode(xP, yP)
+	cQ := clipCode(xQ, yQ)
+	for (cP | cQ) != 0 {
+		if (cP & cQ) != 0 {
+			return
+		}
+		dx := xQ - xP
+		dy := yQ - yP
+		if cP != 0 {
+			if (cP & 8) != 0 {
+				yP += (xMinClip - xP) * dy / dx
+				xP = xMinClip
+			} else if (cP & 4) != 0 {
+				yP += (xMaxClip - xP) * dy / dx
+				xP = xMaxClip
+			} else if (cP & 2) != 0 {
+				xP += (yMinClip - yP) * dx / dy
+				yP = yMinClip
+			} else if (cP & 1) != 0 {
+				xP += (yMaxClip - yP) * dx / dy
+				yP = yMaxClip
+			}
+			cP = clipCode(xP, yP)
+		} else {
+			if (cQ & 8) != 0 {
+				yQ += (xMinClip - xQ) * dy / dx
+				xQ = xMinClip
+			} else if (cQ & 4) != 0 {
+				yQ += (xMaxClip - xQ) * dy / dx
+				xQ = xMaxClip
+			} else if (cQ & 2) != 0 {
+				xQ += (yMinClip - yQ) * dx / dy
+				yQ = yMinClip
+			} else if (cQ & 1) != 0 {
+				xQ += (yMaxClip - yQ) * dx / dy
+				yQ = yMaxClip
+			}
+			cQ = clipCode(xQ, yQ)
+		}
+	}
+	Move(xP, yP)
+	Draw(xQ, yQ)
+}
