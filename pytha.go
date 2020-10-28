8cda9d8ada64032d570d735be9b7f9827a28c25b
Justin Flude <jflude@users.noreply.github.com>
2020-10-27 23:37:49 -0500

    Pythagoras' tree

---
+++ pytha.go
@@ -0,0 +1,41 @@
+// Pythagoras' tree.
+package main
+
+import (
+	"fmt"
+	"github.com/jflude/ppicg/grsys"
+	"os"
+)
+
+func Pythagoras(A, B grsys.Vec, n int) {
+	if n <= 0 {
+		return
+	}
+	C := B.Add(grsys.Vec{X: A.Y - B.Y, Y: B.X - A.X})
+	D := A.Add(C.Sub(B))
+	E := D.Add(C.Sub(A).Mul(0.5))
+	A.Move()
+	B.Draw()
+	C.Draw()
+	D.Draw()
+	A.Draw()
+	Pythagoras(D, E, n-1)
+	Pythagoras(E, C, n-1)
+}
+
+func main() {
+	fmt.Print("Enter xA, yA, xB, yB and recursion depth n: ")
+	var A, B grsys.Vec
+	var n int
+	if _, err := fmt.Scanln(&A.X, &A.Y, &B.X, &B.Y, &n); err != nil {
+		grsys.Error(err)
+	}
+	// Optional argument for output to HP-GL file
+	if len(os.Args) > 1 {
+		grsys.InitGr(os.Args[1])
+	} else {
+		grsys.InitGr("")
+	}
+	Pythagoras(A, B, n)
+	grsys.EndGr()
+}
